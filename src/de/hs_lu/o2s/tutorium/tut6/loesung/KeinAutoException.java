830299293fbd81db6113d5ac5215014f9d0f9cf1
Patrick <Patrick@192.168.0.192>
2019-11-04 13:03:09 +0100

    upload ueb 6

---
+++ src/de/hs_lu/o2s/tutorium/tut6/loesung/KeinAutoException.java
@@ -0,0 +1,14 @@
+package de.hs_lu.o2s.tutorium.tut6.loesung;
+
+public class KeinAutoException extends Exception {
+
+	public KeinAutoException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public KeinAutoException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/de/hs_lu/o2s/tutorium/tut6/uebung/AppAutofahren.java
+++ src/de/hs_lu/o2s/tutorium/tut6/uebung/AppAutofahren.java
@@ -4,29 +4,7 @@ public class AppAutofahren {
 
 	public static void main(String args[]) {
 
-		Auto a1 = new Auto("Mercedes", "C-Klase", 70, new Autofahrer("Thilo Kehrer", true), 50000);
-		Auto a2 = new Auto("BMW", "e30", 420, new Autofahrer("Tim Wiese", false), 220000);
 
-		try {
-			a1.autoFahren(500);
-			System.out.println("Der neue Kilometerstand beträgt: "+a1.getKilometerstand());
-		} catch (KeinFuehrerscheinException kfex) {
-			// TODO: handle exception
-			System.out.println(kfex.getMessage());
-		}
-		
-		try {
-			a2.autoFahren(1200);
-			System.out.println("Der neue Kilometerstand beträgt: "+a2.getKilometerstand());
-		} catch (KeinFuehrerscheinException e) {
-			// TODO: handle exception
-			System.out.println(e.getMessage());
-		}
-		
-		
-		Autofahrer Af0 = new Autofahrer("Oliver Kahn", false);
-		a1.setFahrer(Af0);
-		
 		
 	}
 }
